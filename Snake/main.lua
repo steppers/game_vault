a9d8a08aab662f51adc278bc7304a9c603019c4f
Oliver Steptoe <oliver.steppers@gmail.com>
2020-08-12 23:49:18 +0100

    Create main.lua

---
+++ Snake/main.lua
@@ -0,0 +1,15 @@
+function CartridgeSetup()
+   
+end
+
+function CartridgeUpdate()
+    
+end
+
+function CartridgeDraw()
+    background(255, 0, 255)
+end
+
+function CartridgeOnButton(btn, state)
+   
+end
